531c4171f884a4dbb1dfc58290b9d8a2b8990969
Rony <stamiranda@gmail.com>
2016-04-26 20:13:41 -0400

    Update woof.rb

--- woof.rb
+++ woof.rb
@@ -1,28 +1,31 @@
 def initial_conversation()
 
-puts "A dialogue between my dog and I..."
-puts "ME: "
+  loop do
 
-print "> "
-say = $stdin.gets.chomp.downcase
+    puts "----------------------------------"
+    puts "A dialogue between my dog and I..."
+    puts "ME: "
 
-while not say.empty?
+    print "> "
+    say = $stdin.gets.chomp.downcase
 
-if say == "woof"
-  puts "DOG (barks three times): 'WOOF! WOOF! WOOF!"
-elsif say == "shakil stop" || say == "shakil stop!"
-  puts "DOG: zzz..."
-elsif say == "meow"
-  puts "DOG (barks five times): 'woof! woof! woof! woof! woof!"
-elsif say == "go away"
+    if say == "woof"
+      puts "DOG (barks three times): 'WOOF! WOOF! WOOF!"
+    elsif say == "shakil stop" || say == "shakil stop!"
+      puts "DOG: zzz..."
+    elsif say == "meow"
+      puts "DOG (barks five times): 'woof! woof! woof! woof! woof!"
+    elsif say == "go away"
 
-elsif say.include? "treat"
-  puts "Dog's thinking he’ll be getting a treat..." 
-else
-  puts "Dog barks back once : 'woof!"
-end
+    elsif say.include? "treat"
+      puts "Dog's thinking he’ll be getting a treat..." 
+    else
+      puts "Dog barks back once : 'woof!"
+    end
+    
+    break if say == "false"
+  end 
 
 end
 
 initial_conversation()
-
